661206169c83c5029e2785dc74811043ca02f3cc
Corrin Lakeland <corrin.lakeland@cmeconnect.com>
2026-06-21 13:40:19 +1200

    fix: strip generated ios push entitlement

--- plugins/withoutPushNotifications.js
+++ plugins/withoutPushNotifications.js
@@ -1,8 +1,37 @@
-const { withEntitlementsPlist } = require('expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+const plist = require('@expo/plist').default;
+const { withFinalizedMod } = require('expo/config-plugins');
 
 module.exports = function withoutPushNotifications(config) {
-  return withEntitlementsPlist(config, (config) => {
-    delete config.modResults['aps-environment'];
-    return config;
-  });
+  return withFinalizedMod(config, [
+    'ios',
+    (config) => {
+      removePushEntitlement(config.modRequest.platformProjectRoot);
+      return config;
+    },
+  ]);
 };
+
+function removePushEntitlement(iosProjectRoot) {
+  const entitlementsPaths = fs
+    .readdirSync(iosProjectRoot, { withFileTypes: true })
+    .filter((entry) => entry.isDirectory())
+    .flatMap((entry) => {
+      const directory = path.join(iosProjectRoot, entry.name);
+      return fs
+        .readdirSync(directory, { withFileTypes: true })
+        .filter((file) => file.isFile() && file.name.endsWith('.entitlements'))
+        .map((file) => path.join(directory, file.name));
+    });
+
+  if (entitlementsPaths.length === 0) {
+    throw new Error('Expected Expo prebuild to generate an iOS entitlements file.');
+  }
+
+  for (const entitlementsPath of entitlementsPaths) {
+    const entitlements = plist.parse(fs.readFileSync(entitlementsPath, 'utf8'));
+    delete entitlements['aps-environment'];
+    fs.writeFileSync(entitlementsPath, plist.build(entitlements));
+  }
+}
